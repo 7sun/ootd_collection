fcc0df7d76991124fd83075aa2808c93de939c4d
7sun <pakrouse@gmail.com>
2015-04-14 15:59:30 -0700

    adds user index, new and create methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+  def index
+    # Should only be visible to admin. Need to update.
+    @users = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      if @user.save
+        session[:user_id] = user.id.to_s
+        flash[:info] = "Welcome to OOTD"
+        redirect_to root_path
+      end
+    else
+      # Need to add forget password function
+      flash[:error] = "This user has already been registered. Please login instead"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+    include ActiveModel::SecurePassword
+    has_secure_password
+
+    
 end
