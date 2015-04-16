53a5cade133d966fd19af66f28f5478aaa89a17a
7sun <pakrouse@gmail.com>
2015-04-15 19:36:16 -0700

    adds pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   
-  root 'application#index'
+  root 'pages#index'
   
   get '/login'        => 'sessions#new', as: 'login'
   post '/login'       => 'sessions#create'
