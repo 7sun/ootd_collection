eeb1f9c06a4f62f4c92cc8c30f09634d08633447
7sun <pakrouse@gmail.com>
2015-04-17 15:30:01 -0700

    moves product listings to a partial. Adds product seeds

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   get '/login'        => 'sessions#new', as: 'login'
   post '/login'       => 'sessions#create'
   get '/logout'       => 'sessions#destroy', as: 'logout'
+  get '/tops'         => 'products#tops', as: 'tops'
+  get '/pants'        => 'products#pants', as: 'pants'
+  get '/skirts'       => 'products#skirts', as: 'skirts'
+  get '/dresses'      => 'products#dresses', as: 'dresses'
+  get '/jackets'      => 'products#jackets', as: 'jackets'
 
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,57 @@
   admin: false
   )
 end
+
+Product.create(
+  collection: 'Fall 2015',
+  category: 'Tops',
+  style: 'Fancy Top',
+  style_num: 'D53#21',
+  color: 'red',
+  description: "It's a fancy top for fancy occasions"
+  )
+
+Product.create(
+  collection: 'Fall 2015',
+  category: 'Pants',
+  style: 'Classic Pants',
+  style_num: 'D56#21',
+  color: 'blue',
+  description: "So classy. So you."
+  )
+
+Product.create(
+  collection: 'Fall 2015',
+  category: 'Pants',
+  style: 'Modern Pants',
+  style_num: 'D56#21',
+  color: 'black',
+  description: "So classy. So you."
+  )
+
+Product.create(
+  collection: 'Fall 2015',
+  category: 'Skirts',
+  style: 'Summer Skirt',
+  style_num: 'D53#14',
+  color: 'floral',
+  description: "Perfect for when its hot!"
+  )
+
+Product.create(
+  collection: 'Fall 2015',
+  category: 'Skirts',
+  style: 'Short Skirt',
+  style_num: 'D53#14',
+  color: 'floral',
+  description: "Show some leg. Ooh lala."
+  )
+
+Product.create(
+  collection: 'Fall 2015',
+  category: 'Dresses',
+  style: 'Floral Dress',
+  style_num: 'D53#31',
+  color: 'floral',
+  description: "Dress to impress"
+  )
