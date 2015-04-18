518e21219174d3af545d506f851b18be503e450e
7sun <pakrouse@gmail.com>
2015-04-17 18:01:17 -0700

    adds homepage links

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,10 @@ Rails.application.routes.draw do
   get '/login'        => 'sessions#new', as: 'login'
   post '/login'       => 'sessions#create'
   get '/logout'       => 'sessions#destroy', as: 'logout'
-  # get '/tops'         => 'products#tops', as: 'tops'
-  # get '/pants'        => 'products#pants', as: 'pants'
-  # get '/skirts'       => 'products#skirts', as: 'skirts'
-  # get '/dresses'      => 'products#dresses', as: 'dresses'
-  # get '/jackets'      => 'products#jackets', as: 'jackets'
   get '/collection'   => 'collections#index', as: 'collection'
   get '/about'        => 'pages#about', as: 'about'
+  get '/contact'      => 'pages#contact', as: 'contact'
+  get '/stockists'    => 'stockists#index', as: 'stockists'
 
 
 end
