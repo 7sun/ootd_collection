9fb2a0fd764af86808860e26867cd6a06fcd8718
7sun <pakrouse@gmail.com>
2015-04-21 16:43:44 -0700

    adds CRUD functionality to API productes controller

---
+++ app/assets/javascripts/ng-app/main.js
@@ -0,0 +1,46 @@
+angular
+	.module("ootd", ['ngResource'])
+	 .config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] =
+      $('meta[name=csrf-token]').attr('content');
+  }])
+  .controller("productController", ['$scope', '$http', '$resource', function($scope, $http, $resource){
+		var Product = $resource('/api/products/:id' {id:'@id'},
+			{
+				'update': { method: 'patch'}
+			}
+		);
+
+		Product.query(function(data){
+			$scope.products = data;
+		});
+
+		$scope.createProduct = function(){
+			// Need to add attributes for collection and images
+			new Product({
+				collection_id: $scope.newProduct.collection_id
+				category: $scope.newProduct.category,
+				style_num: $scope.newProduct.style_num,
+				style: $scope.newProduct.style,
+				color: $scope.newProduct.color,
+				materials: $scope.newProduct.materials,
+				description: $scope.newProduct.description
+			}).$save(function(data){
+				$scope.products.unshift(data);
+				$scope.newProduct = null
+			});
+		}
+
+		$scope.saveProduct = function(product) {
+			product.$update();
+		}
+
+		$scope.destroyProduct = function(product, index) {
+			$scope.product = Product.get({id: product.id}, function(product){
+				product.$delete();
+				$scope.products.splice(index, 1);
+			});
+		}
+
+
+  }
--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -7,5 +7,44 @@ module Api
       render json: products
     end
 
+    def show
+      product = Product.find(params[:id])
+      render json: product
+    end
+
+    def create
+      product = Product.new(product_params)
+      if product.save
+        render json: product
+      else
+        render json: {errors: product.errors}, status: 422
+      end
+    end
+
+    def update
+      product = Product.find(params[:id])
+      if product.update(product_params)
+        render json: product
+      else
+        render json: {errors: product.errors}, status: 422
+      end
+    end
+
+    def destroy
+      product = Product.find(params[:id])
+      if product.destroy
+        render status: 200
+      else
+        render status: 400
+      end
+    end
+
+
+
+    private
+
+      def product_params
+        params.require(:product).permit(:collection_id, :category, :style_num, :style, :color, :materials, :description, images_attributes: [:id, :source, :description])
+
   end
 end
