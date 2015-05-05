88148e1c1c4c8f060c8884561032e27464eea922
7sun <pakrouse@gmail.com>
2015-05-05 09:49:00 -0700

    refactors most popular list with favorited users

--- app/controllers/admin/favorites_controller.rb
+++ app/controllers/admin/favorites_controller.rb
@@ -1,17 +1,10 @@
 module Admin
   class FavoritesController < ApplicationController
   
-  def index
-    products = Product.all
-    @sorted = products.sort_by { |p| p.favorites.count }.reverse
-  end
-
-
-
-
-
-
-
+    def index
+      products = Product.all
+      @sorted = products.sort_by { |p| p.favorites.count }.reverse
+    end
 
 
   end
