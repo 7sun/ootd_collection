a0ec70e4172521303e8acdcf9d66a036e99a98ad
7sun <pakrouse@gmail.com>
2015-05-06 20:50:39 -0700

    another home template load check

--- app/assets/javascripts/ng-app/home-controller.js
+++ app/assets/javascripts/ng-app/home-controller.js
@@ -1,10 +1,9 @@
 angular
 	.module("ootd")
   .controller("homeController", ['currentUser', '$scope', function(currentUser, $scope){
-  	console.log(currentUser);
   	if(currentUser){
   		$scope.currentUser = currentUser;
-  		console.log("set current user: " + currentUser);
+  		console.log("set current user: " + currentUser)
   	}
   	else {
   		console.log("no current user here!");
