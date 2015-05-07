6db70d963c39d932077773aa07777e70fb56bb17
7sun <pakrouse@gmail.com>
2015-05-06 20:42:36 -0700

    adds current user check logic

--- app/assets/javascripts/ng-app/home-controller.js
+++ app/assets/javascripts/ng-app/home-controller.js
@@ -1,6 +1,13 @@
 angular
 	.module("ootd")
   .controller("homeController", ['currentUser', '$scope', function(currentUser, $scope){
-  	$scope.currentUser = currentUser;
+  	console.log(currentUser);
+  	if(currentUser){
+  		$scope.currentUser = currentUser;
+  		console.log("set current user: " + currentUser);
+  	}
+  	else {
+  		console.log("no current user here!");
+  	}
 
 	}]);
