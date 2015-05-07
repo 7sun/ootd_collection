0376e447eaf967136c4d307d2a114e6610d96354
7sun <pakrouse@gmail.com>
2015-05-07 00:07:57 -0700

    adds main container with top padding

--- app/assets/javascripts/ng-app/home-controller.js
+++ app/assets/javascripts/ng-app/home-controller.js
@@ -2,6 +2,6 @@ angular
 	.module("ootd")
   .controller("homeController", ["currentUser", '$scope', function(currentUser, $scope){
   		$scope.currentUser = currentUser;
-  		console.log("set current user: " + currentUser.id)
+  		console.log("set current user: " + currentUser)
 
 	}]);
