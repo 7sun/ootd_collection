7d83adbc331609926b52feba9534310f46fb18f5
7sun <pakrouse@gmail.com>
2015-05-12 11:41:16 -0700

    adds media queries for product images

--- app/assets/javascripts/animations.js
+++ app/assets/javascripts/animations.js
@@ -2,24 +2,24 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll > 0) {
         $(".sub-header").addClass("shadow");
-        // $("#main-container").animate({
-        //     'paddingTop' : "80px"
-        // });
-        // $(".header-container").animate({
-        //     'padding' : "1em 0 0"
-        // });
+        $("#main-container").css({
+            'paddingTop' : "86px"
+        });
+        $(".header-container").css({
+            'padding' : "1em 0 0"
+        });
         // $(".site-nav").fadeOut();
     }
     else {
         $(".sub-header").removeClass("shadow");
-        // $("#main-container").animate({
-        //     'paddingTop': "120px"
-        // })
+        $("#main-container").css({
+            'paddingTop': "120px"
+        })
 
-        // // .css({paddingTop: "120px"});
-        // $(".header-container").animate({
-        //     'padding' : "1em 0"
-        // });
+        // .css({paddingTop: "120px"});
+        $(".header-container").css({
+            'padding' : "1em 0"
+        });
         // $(".site-nav").fadeIn();
         // console.log("show menu");
     }
