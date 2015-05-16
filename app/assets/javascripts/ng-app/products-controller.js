1794adf05b840996336052a475250901e49a675a
7sun <pakrouse@gmail.com>
2015-05-16 00:25:18 -0700

    fixes favorties bug

--- app/assets/javascripts/ng-app/products-controller.js
+++ app/assets/javascripts/ng-app/products-controller.js
@@ -2,80 +2,80 @@ angular
 	.module("ootd")
   .controller("productsController", ['allProducts', '$rootScope', 'filterFilter', '$scope', '$http', '$timeout', function(allProducts, $rootScope, filterFilter, $scope, $http, $timeout){
 
-      var currentUser = $rootScope.currentUser;
-      $scope.products = [];
-      $timeout(function(){
-        $scope.products = allProducts;
-        // Creates filtered arrays for each product category
-        $scope.productsArray = $scope.products;
-        $scope.topsArray = filterFilter($scope.products, 'tops');
-        $scope.pantsArray = filterFilter($scope.products, 'pants');
-        $scope.skirtsArray = filterFilter($scope.products, 'skirt');
-        $scope.dressesArray = filterFilter($scope.products, 'dresses');
-        $scope.jumpsuitsArray = filterFilter($scope.products, 'jumpsuits');
-        $scope.jacketsArray = filterFilter($scope.products, 'jackets');
-      })
-			
-	    // Called when the heart icon is clicked. 
-		  $scope.toggleFavorite = function(index, product){
-		  	if (window['faves' + [index]] == true){
-		  		destroyFavorite(index, product);
-		  		window['faves' + [index]] = false;
-		  	}
-		  	else {
-		  		createFavorite(index, product);
-		  			// Call the current user API again to ensure the favorites list includes the newly created favorite
-		  			$http.get('/api/currentuser').success(function(data){
-              $rootScope.currentUser = data;
-		  				currentUser = data;
-		  				checkForFavorite();
-		  			})
-		  	}
-		  }
+    $scope.products = [];
+    $timeout(function(){
+      $scope.products = allProducts;
+      // Creates filtered arrays for each product category
+      $scope.productsArray = $scope.products;
+      $scope.topsArray = filterFilter($scope.products, 'tops');
+      $scope.pantsArray = filterFilter($scope.products, 'pants');
+      $scope.skirtsArray = filterFilter($scope.products, 'skirt');
+      $scope.dressesArray = filterFilter($scope.products, 'dresses');
+      $scope.jumpsuitsArray = filterFilter($scope.products, 'jumpsuits');
+      $scope.jacketsArray = filterFilter($scope.products, 'jackets');
+    })
 
-  	  // Creates a new favorite. User ID is added in the rails FavoritesController
-  	  var createFavorite = function(index, product) {
-  	  	$http.post('/api/favorites/', {product_id: product})
-  	  	.success(function(){
-  	  		var idName = "#heart-" + index;
-  				$(idName).addClass("red");
-  	  	});
-  	  }
-
-  	  // Destroys user's favorite. 
-  	  var destroyFavorite = function(index, product) {
-  	  	var userFaves = currentUser.favorites;
-  	  	for(var i = 0; i < userFaves.length; i++) {
-    			if(userFaves[i].product_id == product){
-    				var favorite = userFaves[i];
-    				$http.delete('/api/favorites/' + favorite.id)
-    				.success(function(){
-    					var idName = "#heart-" + index;
-  						$(idName).removeClass("red");
-    				})
-    			}
-    		}
-    	}
-
-
-      // Check if user has favorited the product
-      var checkForFavorite = function(){
-      	for(var i = 0; i < $scope.products.length; i++) {
-      		var faves = $scope.products[i].favorites;
-      		for(var j = 0; j < faves.length; j++) {
-      			if(faves[j].user_id == currentUser.id){
-      				var idName = "#heart-" + [i];
-      				$(idName).addClass("red");
-      				window['faves' + [i]] = true;
-      			}
-      		}
-      	}
+    
+    // Called when the heart icon is clicked. 
+    $scope.toggleFavorite = function(product){
+      var idName = '#heart-' + product;
+      if ( $(idName).hasClass("red") ){
+        destroyFavorite(product);
       }
+      else {
+        createFavorite(product);
+          // Call the current user API again to ensure the favorites list includes the newly created favorite
+          $http.get('/api/currentuser').success(function(data){
+            $rootScope.currentUser = data;
+            // currentUser = data;
+            checkForFavorite();
+          })
+      }
+    }
 
-      $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
-        console.log("checked for favorites on load");
-        checkForFavorite();
+    // Creates a new favorite. User ID is added in the rails FavoritesController
+    var createFavorite = function(product) {
+      $http.post('/api/favorites/', {product_id: product})
+      .success(function(){
+        var idName = "#heart-" + product;
+        $(idName).addClass("red");
       });
+    }
 
-	}]);
+    // Destroys user's favorite. 
+    var destroyFavorite = function(product) {
+      var userFaves = $rootScope.currentUser.favorites;
+      for(var i = 0; i < userFaves.length; i++) {
+        if(userFaves[i].product_id == product){
+          var favorite = userFaves[i];
+          $http.delete('/api/favorites/' + favorite.id)
+          .success(function(){
+            var idName = "#heart-" + product;
+            $(idName).removeClass("red");
+            $http.get('/api/currentuser').success(function(data){
+              $rootScope.currentUser = data;
+            })
+          })
+        }
+      }
+    }
+
+    // Check if user has favorited the product
+    var checkForFavorite = function(){
+      var faves = $rootScope.currentUser.favorites;
+      for(var i = 0; i < faves.length; i++) {
+        var idName = "#heart-" + faves[i].product_id;
+        $(idName).addClass("red");
+        console.log(faves[i].product_id);
+      }
+    }
+    
+    // Waits for ngRpeat to finish before checking for favorites to add red hearts
+    $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
+      console.log("checked for favorites on load");
+      checkForFavorite();
+    });
+
+}]);
+
 
